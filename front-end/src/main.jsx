e9e8affdf365d64fa89c6ac972b50bf8233f7bca
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-12 08:55:20 +0000

    Router setup

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,10 +4,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
