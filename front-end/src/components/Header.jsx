941feaa800cae8e98a4d884d680285b935bcabde
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-15 13:09:32 +0000

    Added Link

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,15 +2,17 @@ import { Logo } from "./Logo";
 import wordmark from "../assets/svg/logo-wordmark.svg";
 import { NavDesktop } from "./NavDesktop";
 import { NavMobile } from "./NavMobile";
-
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <>
       <header className="flex justify-between">
         <Logo src={wordmark} type="wordmark" />
-        <NavDesktop/>
-        <NavMobile/>
+        {/* Toggle button */}
+        <Link>Get in touch</Link>
+        {/* <NavDesktop/>
+        <NavMobile/> */}
       </header>
     </>
   );
