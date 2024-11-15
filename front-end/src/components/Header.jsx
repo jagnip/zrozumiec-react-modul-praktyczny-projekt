cf3753e5b8b030edc417cbfec4132ed6389420d1
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-15 09:15:49 +0000

    NavDesktop created

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { Logo } from "./Logo";
 import wordmark from "../assets/svg/logo-wordmark.svg";
+import { NavMenu } from "./NavDesktop";
+
 
 export function Header() {
   return (
     <>
-      <header>
+      <header className="flex justify-between">
         <Logo src={wordmark} type="wordmark" />
+        <NavMenu/>
       </header>
     </>
   );
---
+++ front-end/src/components/NavDesktop.jsx
@@ -0,0 +1,30 @@
+const routes = [
+  {
+    title: "Projects",
+    href: "#",
+  },
+  {
+    title: "Visuals",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+  {
+    title: "Say hello",
+    href: "#",
+  },
+];
+
+export function NavDesktop() {
+  return (
+    <ul className="flex gap-5 text-sm lg:flex lg:items-center">
+      {routes.map(({ href, title }) => (
+        <li>
+          <a href={href}>{title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
