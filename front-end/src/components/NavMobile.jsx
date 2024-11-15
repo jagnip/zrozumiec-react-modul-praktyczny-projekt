fb393c155b02b9987bb94cda47fc5fd404b49f72
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-15 10:33:29 +0000

    Added Hamburger package

--- front-end/src/components/NavMobile.jsx
+++ front-end/src/components/NavMobile.jsx
@@ -1,24 +1,35 @@
-const routes = [
-    {
-      title: "Projects",
-      href: "#",
-    },
-    {
-      title: "Visuals",
-      href: "#",
-    },
-    {
-      title: "About",
-      href: "#",
-    },
-    {
-      title: "Say hello",
-      href: "#",
-    },
-  ];
+import { useState } from "react";
+import { Slant as Hamburger } from "hamburger-react";
 
+const routes = [
+  {
+    title: "Projects",
+    href: "#",
+  },
+  {
+    title: "Visuals",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+  {
+    title: "Say hello",
+    href: "#",
+  },
+];
 
 export function NavMobile() {
+  const [isOpen, setOpen] = useState(false);
 
-    return <p>hello</p>
+  return (
+    <Hamburger
+      toggled={isOpen}
+      toggle={setOpen}
+      size={24}
+      distance="sm"
+      label="Show menu"
+    />
+  );
 }
