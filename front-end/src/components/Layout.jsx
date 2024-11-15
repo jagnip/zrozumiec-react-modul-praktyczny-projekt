0c792011da193b75fd77da52c11518a92052dfb8
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-15 14:36:05 +0000

    Added hero animations

--- front-end/src/components/Layout.jsx
+++ front-end/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { easings } from "../utils/animations";
+import { easings, textRevealMotion } from "../utils/animations";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { motion } from "framer-motion";
@@ -14,24 +14,43 @@ export function Layout() {
           animate="animate"
           className="mx-auto flex flex-1 flex-col"
         >
-          <motion.h1 className="md:pl-[10%]">We design</motion.h1>
-          <div className="bg-bg-primary z-10 flex items-center">
-            <motion.div
-              initial={{ scaleX: 0 }}
-              animate={{
-                scaleX: 1,
-                transition: { duration: 0.8, ease: easings.easeInOutQuint },
-              }}
-              className="bg-bg-primary-inverted mr-8 hidden h-1 flex-1 origin-left md:block"
-            />
-            <motion.h1 className="z-20">Good enough</motion.h1>
-          </div>
-          <div className="bg-bg-primary z-20">
-            <motion.h1>Digital products</motion.h1>
+          <div>
+            <motion.h1 variants={textRevealMotion(0)} className="md:pl-[10%]">
+              Beautifully
+            </motion.h1>
+            <div className="bg-bg-primary z-10 flex items-center">
+              <motion.div
+                initial={{ scaleX: 0 }}
+                animate={{
+                  scaleX: 1,
+                  transition: { duration: 0.8, ease: easings.easeInOutQuint },
+                }}
+                className="bg-bg-primary-inverted mr-8 hidden h-1 flex-1 origin-left md:block"
+              />
+              <motion.h1 variants={textRevealMotion(0.2)} className="z-20">
+                designed & coded
+              </motion.h1>
+            </div>
+            <div className="bg-bg-primary z-20">
+              <motion.h1 variants={textRevealMotion(0.4)}>
+                digital products
+              </motion.h1>
+            </div>
           </div>
           <div className="bg-bg-primary z-30 w-full flex-1 pt-8 md:pl-[20%]">
-            <motion.p className="max-w-lg">
-              Good Enough Studio® is a design studio focused on digital
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 0.8,
+                  duration: 0.8,
+                  ease: easings.easeInOutQuint,
+                },
+              }}
+              className="max-w-lg"
+            >
+              Blue Berry® is a design studio focused on digital
               products that works with companies around the world, helping them
               grow and expand their business.
             </motion.p>
--- front-end/src/utils/animations.js
+++ front-end/src/utils/animations.js
@@ -2,3 +2,17 @@ export const easings = {
     easeOutQuart: [0.25, 1, 0.5, 1],
     easeInOutQuint: [0.83, 0, 0.17, 1],
   };
+
+  export function textRevealMotion(delay) {
+    return {
+        initial: {y: 100, opacity: 0},
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay, duration: 0.8, ease: easings.easeOutQuart
+            }
+        }
+
+    }
+  }
