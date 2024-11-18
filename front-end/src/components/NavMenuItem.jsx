9a9da576d18f6a22f699687e5f2fe0196a2fe82a
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-18 12:04:17 +0000

    Styled MenuItem

--- front-end/src/components/NavMenuItem.jsx
+++ front-end/src/components/NavMenuItem.jsx
@@ -1,16 +1,19 @@
-import arrow from "../assets/svg/arrow.svg"
+import arrow from "../assets/svg/arrow.svg";
 
 export function NavMenuItem({ index, title }) {
   return (
-    <li className="text-text-primary-inverted">
-      <div>
-        <div />
-        <span>
+    <li className="text-text-primary-inverted relative w-full cursor-pointer py-4">
+      <div className="relative flex w-full items-center">
+        <div className="bg-bg-primary-inverted absolute inset-0 hidden" />
+        <span className="w-[4ch]">
           {index.toLocaleString("en-US", { minimumIntegerDigits: 2 })}
         </span>
-        <h1>{title}</h1>
-        <div>{arrow}</div>
+        <h3 className="text-text-primary-inverted flex-1">{title}</h3>
+        <div>
+          <p>arrow</p>
+        </div>
       </div>
+      <div className="bg-bg-primary absolute bottom-0 h-[2px] w-full origin-left" />
     </li>
   );
 }
