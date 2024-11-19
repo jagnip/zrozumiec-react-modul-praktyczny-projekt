f574048b57d4834a91d5ec6696f70a74f4bb2e05
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2024-11-19 09:03:08 +0000

    Changed svg to sharper

--- front-end/src/components/NavMenuItem.jsx
+++ front-end/src/components/NavMenuItem.jsx
@@ -6,14 +6,20 @@ import {
   itemContentMotion,
   itemCoverMotion,
 } from "../utils/animations";
+import { useState } from "react";
 
 export function NavMenuItem({ index, title }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <motion.li
-      className="text-text-primary-inverted relative w-full cursor-pointer py-4"
+      className={`text-text-primary-inverted relative w-full cursor-pointer py-4 ${
+        isLoading ? "pointer-events-none" : "pointer-events-auto"
+      }`}
       initial="initial"
       animate="animate"
       whileHover="hover"
+      onAnimationComplete={() => setIsLoading(false)}
     >
       <div className="relative flex w-full items-center">
         <motion.div
